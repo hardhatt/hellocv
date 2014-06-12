fc5f5035f5914ca5afd173bee79cbc2759c65a46
hardhatt <kouzitomasao@yahoo.co.jp>
2014-06-12 23:39:40 +0900

    all End

---
+++ CMakeFiles/dspimg.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/dspimg.dir/dspimg.cc.o"
+  "dspimg.pdb"
+  "dspimg"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/dspimg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
